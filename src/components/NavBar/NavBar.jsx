09f601942bc0dff4923713a0a8b09ec67c31bab0
agusrod9 <agusrod9@gmail.com>
2023-04-25 22:18:15 -0300

    Segunda Pre Entrega

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+
+export const NavBar = () => {
+  return (
+    <div className="ContenedorMenu">
+      <ul>
+        <li>Productos</li>
+        <li>Servicios</li>
+        <li>contacto</li>
+      </ul>
+    </div>
+  );
+};
